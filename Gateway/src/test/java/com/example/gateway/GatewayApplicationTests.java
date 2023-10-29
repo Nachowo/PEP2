fd186d3c05057c156b765fd27106b3b1aeaf3223
Nachowo <ignacio.arias.t@usach.cl>
2023-10-28 22:30:52 -0300

    hecho el gateway

---
+++ Gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootTest
+@EnableEurekaClient
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
