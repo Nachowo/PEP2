0ee962ba4e5cb39d8938709fe3dcda00964e96cb
Nachowo <ignacio.arias.t@usach.cl>
2023-10-29 00:14:27 -0300

    intentando solucionar eureka

--- Cuotas2/src/main/java/com/example/cuotas2/Cuotas2Application.java
+++ Cuotas2/src/main/java/com/example/cuotas2/Cuotas2Application.java
@@ -2,8 +2,10 @@ package com.example.cuotas2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Cuotas2Application {
 
     public static void main(String[] args) {
