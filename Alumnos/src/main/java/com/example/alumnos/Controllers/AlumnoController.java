da141788cbdf7d2423e7f2c37884b45e71a4738e
Nachowo <ignacio.arias.t@usach.cl>
2023-11-10 13:41:22 -0300

    dejo de funcionar alumno-service, aparentemente por la db

--- Alumnos/src/main/java/com/example/alumnos/Controllers/AlumnoController.java
+++ Alumnos/src/main/java/com/example/alumnos/Controllers/AlumnoController.java
@@ -8,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/alumno")
@@ -29,17 +27,16 @@ public class AlumnoController {
 
 
     @PostMapping
-        public ResponseEntity<Map<String, Object>> agregarAlumno(@RequestBody AlumnoEntity alumno){
+    public ResponseEntity<Map<String, Object>> agregarAlumno(@RequestBody AlumnoEntity alumno){
         System.out.println("se ingreso a agregar alumno con el siguiente alumno: " + alumno.toString());
-            Map<String, Object> response = new HashMap<>();
-            if (alumno != null){
-                response.put("alumno", alumnoService.agregarAlumno(alumno));
-                response.put("status", true);
-                return ResponseEntity.ok(response);
-            }
-            response.put("status", false);
-            return ResponseEntity.badRequest().body(response);
+        Map<String, Object> response = new HashMap<>();
+        if (alumno != null){
+            response.put("alumno", alumnoService.agregarAlumno(alumno));
+            response.put("status", true);
+            return ResponseEntity.ok(response);
         }
+        return ResponseEntity.badRequest().body(response);
+    }
 
     @GetMapping
     public ResponseEntity<Map<String, Object>> obtenerAlumnos(){
